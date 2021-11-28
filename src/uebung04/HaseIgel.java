14cb86511d1f6472491a6502bc02d1dedb74960c
janderg <jan.jb.bruns@gmail.com>
2021-11-28 21:27:49 +0100

    Update uebung04

--- src/uebung04/HaseIgel.java
+++ src/uebung04/HaseIgel.java
@@ -4,27 +4,27 @@ public class HaseIgel {
     public static void main(String[] args) {
         int startwerte = 0;
         for (int i = 0x0000; i <= 0xFFFF; i += 0x0001) {
-            long hase = i, igel = i; //aktualisieren von Hase und Igel
+            long hase = i, igel = i; //Initialisierung von Hase und Igel
             String hex = Long.toHexString(i);
             hex = hex.toUpperCase();
 
             for (int j = 0x0000; j <= 0x10000; j += 0x0001) {
-                igel = middleSquare(igel); //Igel aktualisieren
+                igel = middleSquare(igel); //Igel middelsquare
                 hase = middleSquare(hase);
-                hase = middleSquare(hase); //Hase aktualisieren
+                hase = middleSquare(hase); //Hase middelsquare
                 if (hase == 0x0000) break;
                 if (igel == hase) {
-                    System.out.println("Startwert in Hexadezimal: 0x" + hex); //Startwert ausgeben
+                    System.out.println("Startwert: 0x" + hex + " = " + i); //Startwert ausgeben
                     startwerte++;
                     break;
                 }
             }
         }
-        System.out.println("Es gibt " + startwerte + " Startwerte");
+        System.out.println("Es gibt " + startwerte + " Startwerte von Folgen mit einem Zyklus > 1");
     }
 
     private static long middleSquare(long var) {
-        var *= var;
-        return var / 0x100 % 0x10000;
+        var *= var; //var quadrieren
+        return var / 0x100 % 0x10000; //var aktualisieren
     }
 }
---
+++ src/uebung05/ polynomial.md
@@ -0,0 +1,13 @@
+# **Informatik I, Übung 04**
+## **Polynomfunktion**
+### **1.**
+
+### **2.**
+a)
+b)
+c)
+d)
+### **3.**
+a)
+b)
+c)
