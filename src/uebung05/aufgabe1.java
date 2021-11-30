203a77477db3b228453e8565ff68e20ebc1a42c9
janderg <jan.jb.bruns@gmail.com>
2021-11-30 19:10:31 +0100

    Update aufgabe1.java und aufgabe2.java

--- src/uebung05/aufgabe1.java
+++ src/uebung05/aufgabe1.java
@@ -8,18 +8,25 @@ public class aufgabe1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+        System.out.println("Obere Grenze(i): ");
         int n = scanner.nextInt();
+        System.out.println("Untere Grenze(j):");
+        int j = scanner.nextInt();
         int[] arr = new int[n];
-        int sum = 0;
+        int[] prefsum = new int[n];
+        int sum, sumj = 0, sumi = 0;
         if (n > 1) {
             for (int i = 0; i < n; i++) {
-                arr[i] = random.nextInt(1,n);
-                sum+= arr[i];
+                arr[i] = random.nextInt(1, n);
+                prefsum[i] = sumi + arr[i];
+                sumi = prefsum[i];
+                if (i < j) {
+                    sumj = sumi;
+                }
             }
+            sum = sumi - sumj;
             System.out.println("Meine Zufallszahlen sind: " + Arrays.toString(arr));
-            System.out.println("Die Summe aus arr["+n+"] = "+sum);
-        } else {
-            System.out.println("Fehler, die Applikation wird beendet");
-        }
+            System.out.println("prefsum[i] = " + sum);
+        } else System.out.println("Fehler, die Applikation wird beendet");
     }
 }
--- src/uebung05/aufgabe2.java
+++ src/uebung05/aufgabe2.java
@@ -8,21 +8,23 @@ public class aufgabe2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        int n = scanner.nextInt();
+        int n = scanner.nextInt(), aMean, aMeanSum = 0;
         int[] arr = new int[n];
-        int aMean;
-        int gMean = 0;
-        int aMeanSum = 0;
+        double gMean, gMeanSum = 1;
         if (n > 1) {
             for (int i = 0; i < n; i++) {
-                arr[i] = random.nextInt(1,n);
+                arr[i] = random.nextInt(1, n + 1);
                 aMeanSum += arr[i];
-
+                gMeanSum *= arr[i];
             }
-            System.out.println("Meine Zufallszahlen["+n+"] sind: " + Arrays.toString(arr));
-            aMean = aMeanSum/n;
-            System.out.println("Die Summe aus Zufallszahlen["+n+"] ist: "+aMeanSum);
-            System.out.println("Das arithmetische Mittel von: "+aMeanSum+"/"+n+" ist " +aMean);
-        }else System.out.println("FEHLER; ABBRUCH!!!!!!");
+            System.out.println("Meine Zufallszahlen[" + n + "] sind: " + Arrays.toString(arr));
+            aMean = aMeanSum / n;
+            gMean = (int) Math.pow(aMeanSum, 1.0 / n);
+            System.out.println("Das arithmetische Mittel von: " + aMeanSum + " ist " + aMean);
+            System.out.println("Das geometrische Mittel von " + gMeanSum + " ist " + gMean);
+            if (aMean >= gMean) {
+                System.out.println("OK");
+            } else System.out.println("WIDERLEGT");
+        } else System.out.println("FEHLER!!! !1!ABBRUCH!!11!");
     }
 }
