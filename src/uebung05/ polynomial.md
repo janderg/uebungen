ec62f612d196bea0da226c3440583a4c925ad7f8
janderg <jan.jb.bruns@gmail.com>
2021-12-05 22:12:20 +0100

    Update aufgabe1.java und polynomial.md

--- src/uebung05/ polynomial.md
+++ src/uebung05/ polynomial.md
@@ -10,13 +10,51 @@
     }
     return q;
     
-Es werden [] Multiplikationen und Additionen ausgeführt.
-### **2.**
+Addition: n
+
+Multiplikation: $n+sum_(i=1)^n i*i$
+### **2.** 
 a)
+
+$q =(...((a_n)x^((n^2)-(n-1)^2)+a_(n-1))x^((n-1)^2-(n-2)^2)+...)x^(1^2)+a_0 $
+
 b)
+
+Die Potenzen von x können in einem Array mit größe N gespeichert werden und die Potenzen selbst durch: $Potenz[n] = 1 + n*2 + Potenz[n-1]$ berechnet werden.
+
 c)
+
+Eingabe: Koefizienten: a[n],..,a[o] und $x$
+
+    Array [n] exp;
+    q = a [n];
+    for (i = 1; i <= n; i++) {
+    exp [i] = 1 + (i-1) * 2
+    }
+    for (j = n; j >= 1; j--) {
+    q = q * Math.pow(x,exp[j]) + a[j-1];
+    }
+    return q;
+
 d)
+
+2n Additionen, 2n Multiplikationen
 ### **3.**
-a)
-b)
-c)
+**a)**
+
+$x^(n * (n + 1) * (2 * n + 1) / 6)$
+
+**b)**
+
+Eingabe: Argument x, n ($n > 0$)
+
+    long pow = (n * (n + 1) * (2 * n + 1) / 6);
+    long q = 1;
+    for (long i = 0; i < pow; i++)
+        q *= x;
+
+**c)**
+
+Addition: 2
+
+Multiplikation: (n * (n + 1) * (2 * n + 1) / 6) + 3
--- src/uebung05/aufgabe1.java
+++ src/uebung05/aufgabe1.java
@@ -7,26 +7,32 @@ import java.util.Scanner;
 public class aufgabe1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
         Random random = new Random();
-        System.out.println("Obere Grenze(i): ");
+        System.out.print("Zahl > 1: ");
         int n = scanner.nextInt();
-        System.out.println("Untere Grenze(j):");
-        int j = scanner.nextInt();
-        int[] arr = new int[n];
-        int[] prefsum = new int[n];
-        int sum, sumj = 0, sumi = 0;
+        int[] arr = new int[n], prefsum = new int[n];
+        int  pref = 0;
         if (n > 1) {
             for (int i = 0; i < n; i++) {
                 arr[i] = random.nextInt(1, n);
-                prefsum[i] = sumi + arr[i];
-                sumi = prefsum[i];
-                if (i < j) {
-                    sumj = sumi;
-                }
+                pref += arr[i];
+                prefsum[i] = pref;
             }
-            sum = sumi - sumj;
             System.out.println("Meine Zufallszahlen sind: " + Arrays.toString(arr));
-            System.out.println("prefsum[i] = " + sum);
+            System.out.println("prefsum[n] = " + Arrays.toString(prefsum));
+            System.out.println("Bitte übergebe nun jeweils zwei einzelne Werte i & j mit folgenden Eigenschaften: (i > 0 & i < j & j < n)");
+            System.out.println("i:");
+            int i = scanner1.nextInt();
+            System.out.println("j:");
+            int j = scanner2.nextInt();
+            if (i < 0 || i >= j || j >= n) {
+                System.out.println("Deine Werte erfüllen nicht die Eigenschaften!");
+                return;
+            }
+            int sum = prefsum[j] - (i == 0 ? 0 : prefsum[i - 1]);
+            System.out.println("Die Summe vom Intervall arr[" + i + ", " + j + "] ist: " + sum);
         } else System.out.println("Fehler, die Applikation wird beendet");
     }
 }
